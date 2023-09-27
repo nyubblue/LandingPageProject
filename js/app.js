78f91ab26ceb02a97f7aae1d45f2b8645abbbe77
buynlt0101 <buynlt0101@gmail.com>
2023-09-27 21:47:55 +0700

    commit source landingpage

---
+++ js/app.js
@@ -0,0 +1,89 @@
+/**
+ * Define Global Variables
+ *
+ */
+const navbar = document.getElementById('navbar__list');
+const headerE = document.querySelector('.page__header');
+const sections = document.querySelectorAll('.section');
+const btnTopE = document.getElementById('topbtn');
+let hiddenNavTimer;
+
+/**
+ * End Global Variables
+ * Begin for declare Functions
+ *
+ */
+function genNavBar() {
+    const liMenu = document.createDocumentFragment()
+    for (let i = 1; i <= sections.length; i++) {
+        const liElemt = document.createElement('li')
+        const aElem = document.createElement('a')
+        aElem.textContent = 'Section ' + i
+        aElem.classList.add('menu__link')
+        aElem.setAttribute('id', 'section-' + i)
+        aElem.href
+        liElemt.appendChild(aElem)
+        liMenu.appendChild(liElemt)
+    }
+    navbar.appendChild(liMenu)
+}
+
+/**
+ * End for declare Functions
+ * Begin Main
+ *
+ */
+
+// build the nav
+genNavBar()
+
+// Scroll to anchor ID using scrollTO
+btnTopE.addEventListener('click', e => {
+    e.preventDefault
+    window.scrollTo(0, 0)
+})
+
+// Add class 'active' to section when near top of viewport
+document.addEventListener('scroll', e => {
+    e.preventDefault
+
+    // Display Go Top Button when the user scrolls below the fold of the page.
+    if (this.scrollY < window.innerHeight) {
+        btnTopE.style.display = 'none'
+    } else {
+        btnTopE.style.display = 'block'
+    }
+
+    // Hide fixed navigation bar while not scrolling
+    headerE.classList.add('fixed_menu')
+    clearTimeout(hiddenNavTimer)
+    btnTopE.style.top = ''
+    hiddenNavTimer = setTimeout(() => {
+        headerE.classList.remove('fixed_menu')
+        btnTopE.style.top = '0'
+    }, 2000)
+
+    sections.forEach(section => {
+        const box = section.getBoundingClientRect()
+        section.classList.remove('active-cls')
+        if (box.top <= 150 && box.bottom >= 150) {
+            section.classList.add('active-cls')
+        }
+    })
+})
+
+// Scroll to section on link click
+navbar.addEventListener('click', function (e) {
+    e.preventDefault;
+    const target = e.target
+    if (target.nodeName == 'A') {
+        const secId = target.getAttribute('id');
+        const activeElem = document.querySelector(`[data-nav='${secId}'`);
+        window.scrollTo(0, activeElem.offsetTop + (headerE.classList.contains('fixed_menu') ? headerE.offsetHeight : 0));
+    }
+})
+
+/**
+ * End Main
+ *
+ */
